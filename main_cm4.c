8381757dc704dcb82ba6d2adfc63ce07fbc6f5b2
Yurii Zinchuk <zinchukyurii25@gmail.com>
2023-01-08 15:30:31 +0200

    writing adc codes, reduced sampling freq and recording time. Added amplitudes scaling and adjusting on average level

--- main_cm4.c
+++ main_cm4.c
@@ -9,20 +9,28 @@
  *
  * ========================================
 */
+
 #include "project.h"
 #include <stdio.h>
 
 #define UART_HW ((CySCB_Type *) UART_SCB__HW)
 
-#define FREQ 12000
-#define TIME 10
+#define FREQ 8000
+#define TIME 8
 #define SAMPLES FREQ*TIME
 
 int write_voice();
-int play_voice();
+int get_codes();
+int transform_codes();
 
-int8_t voice[SAMPLES];
-//int8_t codes[SAMPLES];
+int play_voice();
+void no_recording_handler();
+
+uint16_t average;
+bool recorded = false;
+int16_t codes[SAMPLES];
+//int8_t voice[SAMPLES];
+
 
 int main(void)
 {
@@ -44,37 +52,22 @@ int main(void)
                 write_voice();
             }
             if (CapSense_IsWidgetActive(CapSense_BUTTON1_WDGT_ID)) {
-                play_voice();
+                if (recorded) play_voice();
+                else no_recording_handler();
             }
         }
     }
 }
+
 
 int write_voice()
 {
     Cy_GPIO_Write(BLUE_PORT, BLUE_NUM, 0);
     printf("Started voice recording...\r\n");
     
-    ADC_Start();
-    ADC_StartConvert();
-    
-    for(int idx = 0; idx < SAMPLES; ++idx){
-        ADC_IsEndConversion(CY_SAR_WAIT_FOR_RESULT);
-        int16_t ADC_Result = ADC_GetResult16(0);
-
-        int16_t Mic_mV = ADC_CountsTo_mVolts(0, ADC_Result);
-        voice[idx] = (int8_t) Mic_mV - 1600;
-        
-        //Only codes, no mV
-        /*
-        //check what is default adc code, subtract it
-        int8_t code = ADC_Result;
-        codes[idx] = code;
-        */
-    }
-    
-    ADC_StopConvert();
-    ADC_Stop();
+    get_codes();
+    transform_codes();
+    recorded = true;
     
     Cy_GPIO_Write(BLUE_PORT, BLUE_NUM, 1);
     printf("Finished voice recording.\r\n\n");
@@ -82,29 +75,79 @@ int write_voice()
     return 0;
 }
 
+
+int get_codes()
+{
+    ADC_Start();
+    ADC_StartConvert();
+    
+    for (int i = 0; i < SAMPLES; ++i)
+    {
+        ADC_IsEndConversion(CY_SAR_WAIT_FOR_RESULT);
+        uint16_t ADC_Result = ADC_GetResult16(0);
+
+        codes[i] = ADC_Result;
+
+        /*
+        int16_t Mic_mV = ADC_CountsTo_mVolts(0, ADC_Result);
+        voice[i] = (int8_t) Mic_mV - 1600;
+        */
+    }
+    
+    ADC_StopConvert();
+    ADC_Stop();
+    
+    return 0;
+}
+
+
+int transform_codes()
+{
+    uint32_t sum = 0;
+    for (int i = 0; i < SAMPLES; ++i)
+        sum += codes[i];
+        
+    average = sum / SAMPLES;
+
+    uint16_t max_amp = 0;
+    for (int i = 0; i < SAMPLES; ++i)
+    {
+        codes[i] = codes[i] - average;
+
+        if (codes[i] > 0 && codes[i] > max_amp)
+            max_amp = codes[i];
+        else if (codes[i] < 0 && -codes[i] > max_amp)
+            max_amp = -codes[i];
+    }
+    
+    for (int i = 0; i < SAMPLES; ++i)
+        codes[i] = 2048 + (2047 * codes[i] / max_amp);
+        
+    return 0;
+}
+
+
 int play_voice()
 {
     Cy_GPIO_Write(RED_PORT, RED_NUM, 0);
     printf("Started voice playing...\r\n");
+    
     VDAC_1_Start();
 
-    for(int idx = 0; idx < SAMPLES; idx++) {
+    for (int i = 0; i < SAMPLES; ++i)
+    {
+        VDAC_1_SetValue(codes[i]);
+        
+        CyDelayUs(125);
+        
         /*
         char8 text[8];
         sprintf(text, "%d", voice[idx]);
         printf("%s\r\n", text);
-        */
-        
+
         int32_t value = 10*voice[idx] + 1650;
-        VDAC_1_SetValue(value);
-        
-        //Only codes, no mV
-        /*
-        int32_t code = codes[idx] + 2048;
-        VDAC_1_SetValue(value);
+        int32_t value = 2048 + codes[idx];
         */
-        
-        CyDelayUs(83);
     }
     
     VDAC_1_Stop();
@@ -113,6 +156,20 @@ int play_voice()
     printf("Finished voice playing.\r\n\n");
 
     return 0;
+}
+
+
+void no_recording_handler()
+{
+    printf("No recording. Record voice first.\r\n\n");
+
+    for (int i = 0; i < 3; ++i) {
+        Cy_GPIO_Write(RED_PORT, RED_NUM, 0);
+        CyDelay(150);
+        
+        Cy_GPIO_Write(RED_PORT, RED_NUM, 1);
+        CyDelay(150);
+    }
 }
 
 /* [] END OF FILE */
